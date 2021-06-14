42bfd26f5a4dd0ca96660e3489a67e0ba2df5965
“Zike-sbr” <kheiro.92@live.fr>
2021-06-14 21:09:24 +0100

    commit

--- final.js
+++ final.js
@@ -47,35 +47,40 @@ function moveToPrevSlide() {
 // validate the email adress
 
 
-function validateEmail(){
-var x=document.forms["loginForm"]["email"].value;
-var atpos=x.indexOf("@");
-var dotpos=x.lastIndexOf(".");
+// function validateEmail(){
+// var x=document.forms["loginForm"]["email"].value;
+// var atpos=x.indexOf("@");
+// var dotpos=x.lastIndexOf(".");
 
-var name = document.loginForm.name.value
-   if (name.length==0)
-    return alert(`name is required`)
+// var name = document.loginForm.name.value
+//    if (name.length==0)
+//     return alert(`name is required`)
 
-var password = document.loginForm.password.value
-if (password.length<8)
-    return alert(`password length should more than 8`)
+// var password = document.loginForm.password.value
+// if (password.length<8)
+//     return alert(`password length should more than 8`)
 
-if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length){
-alert("Please enter a valid email address.");
-return false;
-}
+// if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length){
+// alert("Please enter a valid email address.");
+// return false;
+// }
 
-}
+// }
 
 
 
 //create an account 
 
 
-let compte = document.querySelector('done');
-compte.addEventListener('click', () => {
-    //don't know what to put inside
+function created(){
     
-})
+    alert("Your accound has been created succesfully");
+}
 
-// once home i'll do it 
+// calling
+
+// function calling(){
+    
+// }
+
+// keep it for later
